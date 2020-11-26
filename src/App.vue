bbe1c570511c51871dba58a47befa2c08b9805d9
Alexandr Volmanov <C:\Users\al\AppData\Roaming\The Bat!>
2020-11-26 19:15:59 +0300

    new repo

---
+++ src/App.vue
@@ -0,0 +1,434 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <button class="btn btn-success" @click="doStart()">Запуск</button>
+      <button class="btn btn-danger" @click="doPause()">Остановка</button>
+      <button class="btn btn-warning" @click="reset()">Сброс</button>
+    </div>
+    <h2>Сумма: {{ total }} BTC</h2>
+    <div v-if="transactions.length === 0">Empty</div>
+    <table class="table" v-if="transactions.length !== 0">
+      <thead>
+        <tr>
+          <th>From</th>
+          <th>To</th>
+          <th>Sum</th>
+        </tr>
+      </thead>
+      <transition-group tag="tbody" name="list">
+        <tr class="list-item" v-for="(transaction, index) in transactions" :key="transaction.hash + index">
+          <td>
+            <div v-for="(ts, i) in transaction.sender" :key="ts.script + i">{{ ts.addr }}</div>
+          </td> 
+          <td>
+            <div v-for="(tr, i) in transaction.reciev" :key="tr.script + i">{{ tr.prev_out.addr }}</div>
+          </td>
+          <td>
+            {{ transaction.sum }} BTC
+          </td> 
+        </tr>
+      </transition-group>
+    </table>
+  </div>
+</template>
+<script>
+
+import { apiUrl, apiSubscribe, apiUnSubscribe } from './secret'; 
+export default {
+  data () {
+    return {
+      total: 0,
+      connected: null,
+      transactions: []
+    }
+  },
+  methods: {
+    getRecievSum (array) {
+      let sum = 0;
+      for (var i = array.length - 1; i >= 0; i--) {
+        sum += array[i].prev_out.value;
+      }
+
+      return parseFloat(sum * 0.00000001);
+    },
+    getSenderSum (array) {
+      let sum = 0;
+      for (var i = array.length - 1; i >= 0; i--) {
+        sum += array[i].value;
+      }
+
+      return parseFloat(sum * 0.00000001);
+    },
+    doStart () {
+      this.connection.send(JSON.stringify(apiSubscribe));
+    },
+    doPause () {
+      this.connection.send(JSON.stringify(apiUnSubscribe));
+    },
+    reset () {
+      this.transactions = [];
+      // this.connection.close();
+    }
+  },
+  created() {
+    this.connection = new WebSocket(apiUrl);
+    this.connection.onmessage = (event) => {
+      if (JSON.parse(event.data)) {
+
+        let json = JSON.parse(event.data).x;
+        if (json.inputs && json.out) {
+
+          // Тут я пытался сделать через reduce но после долгих отладок решил сделать быстро, думаю стоит рефакторить.
+          // let sumReciev = json.inputs.length > 1 ? json.inputs.reduce((a, b) => {(a.prev_out.value += b.prev_out.value) * 0.00000001}, 0) : json.inputs[0].prev_out.value  * 0.00000001;
+          // let sumSender = json.out.length > 1 ? json.out.reduce((a, b) => a.value + b.value) : json.out[0].value; 
+          // 
+          let sumReciev = this.getRecievSum(json.inputs);
+          let sumSender = this.getSenderSum(json.out);
+          let sum = parseFloat((sumReciev + sumSender).toFixed(8));
+
+          // Я очень не уверен что правильно беру значения, и не понимаю почему в транзакции могу быть несколько отправляющих и получающих
+          // Поэтому складываю оба значения из out и inputs
+          // Довольно простое задание, хоть особо не работал с сокетами, документация простая - все понятно.
+          // this.transactions = [...[{reciev: json.inputs, sender: json.out, hash: json.hash, sum: parseFloat(sumReciev).toFixed(8) + parseFloat(sumSender).toFixed(8)}], ...this.transactions]; //reverse
+          this.transactions = [...this.transactions, ...[{reciev: json.inputs, sender: json.out, hash: json.hash, sum: sum}]];
+          this.total = parseFloat((this.total + sum).toFixed(8));
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.header {
+  text-align: center;
+}
+
+.header .btn {
+  display: inline-block;
+  min-width: 150px;
+  margin: 0 30px;
+}
+
+.table  {
+  border-collapse: collapse;
+  width: 90%;
+  margin: 0 auto;
+  text-align: left;
+}
+
+.table tr th {
+  padding: 15px 10px;
+  font-size: 13px;
+  color: #999;
+}
+
+.table tr td {
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+  padding: 15px 10px;
+  line-height: 2;
+  font-size: 16px;
+  overflow: hidden;
+}
+
+.table tr th:first-child {
+  width: 35%;
+}
+
+.table tr th:last-child {
+  width: 30%;
+}
+
+
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
+/* bootstrap btns */
+.btn {
+  display: inline-block;
+  font-weight: 400;
+  line-height: 1.5;
+  color: #212529;
+  text-align: center;
+  text-decoration: none;
+  vertical-align: middle;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out, border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+}
+.btn:hover {
+  color: #212529;
+}
+.btn-check:focus + .btn, .btn:focus {
+  outline: 0;
+  box-shadow: 0 0 0 0.25rem rgba(13, 110, 253, 0.25);
+}
+.btn:disabled, .btn.disabled, fieldset:disabled .btn {
+  pointer-events: none;
+  opacity: 0.65;
+}
+
+.btn-primary {
+  color: #fff;
+  background-color: #0d6efd;
+  border-color: #0d6efd;
+}
+.btn-primary:hover {
+  color: #fff;
+  background-color: #0b5ed7;
+  border-color: #0a58ca;
+}
+.btn-check:focus + .btn-primary, .btn-primary:focus {
+  color: #fff;
+  background-color: #0b5ed7;
+  border-color: #0a58ca;
+  box-shadow: 0 0 0 0.25rem rgba(49, 132, 253, 0.5);
+}
+.btn-check:checked + .btn-primary, .btn-check:active + .btn-primary, .btn-primary:active, .btn-primary.active, .show > .btn-primary.dropdown-toggle {
+  color: #fff;
+  background-color: #0a58ca;
+  border-color: #0a53be;
+}
+.btn-check:checked + .btn-primary:focus, .btn-check:active + .btn-primary:focus, .btn-primary:active:focus, .btn-primary.active:focus, .show > .btn-primary.dropdown-toggle:focus {
+  box-shadow: 0 0 0 0.25rem rgba(49, 132, 253, 0.5);
+}
+.btn-primary:disabled, .btn-primary.disabled {
+  color: #fff;
+  background-color: #0d6efd;
+  border-color: #0d6efd;
+}
+
+.btn-secondary {
+  color: #fff;
+  background-color: #6c757d;
+  border-color: #6c757d;
+}
+.btn-secondary:hover {
+  color: #fff;
+  background-color: #5c636a;
+  border-color: #565e64;
+}
+.btn-check:focus + .btn-secondary, .btn-secondary:focus {
+  color: #fff;
+  background-color: #5c636a;
+  border-color: #565e64;
+  box-shadow: 0 0 0 0.25rem rgba(130, 138, 145, 0.5);
+}
+.btn-check:checked + .btn-secondary, .btn-check:active + .btn-secondary, .btn-secondary:active, .btn-secondary.active, .show > .btn-secondary.dropdown-toggle {
+  color: #fff;
+  background-color: #565e64;
+  border-color: #51585e;
+}
+.btn-check:checked + .btn-secondary:focus, .btn-check:active + .btn-secondary:focus, .btn-secondary:active:focus, .btn-secondary.active:focus, .show > .btn-secondary.dropdown-toggle:focus {
+  box-shadow: 0 0 0 0.25rem rgba(130, 138, 145, 0.5);
+}
+.btn-secondary:disabled, .btn-secondary.disabled {
+  color: #fff;
+  background-color: #6c757d;
+  border-color: #6c757d;
+}
+
+.btn-success {
+  color: #fff;
+  background-color: #198754;
+  border-color: #198754;
+}
+.btn-success:hover {
+  color: #fff;
+  background-color: #157347;
+  border-color: #146c43;
+}
+.btn-check:focus + .btn-success, .btn-success:focus {
+  color: #fff;
+  background-color: #157347;
+  border-color: #146c43;
+  box-shadow: 0 0 0 0.25rem rgba(60, 153, 110, 0.5);
+}
+.btn-check:checked + .btn-success, .btn-check:active + .btn-success, .btn-success:active, .btn-success.active, .show > .btn-success.dropdown-toggle {
+  color: #fff;
+  background-color: #146c43;
+  border-color: #13653f;
+}
+.btn-check:checked + .btn-success:focus, .btn-check:active + .btn-success:focus, .btn-success:active:focus, .btn-success.active:focus, .show > .btn-success.dropdown-toggle:focus {
+  box-shadow: 0 0 0 0.25rem rgba(60, 153, 110, 0.5);
+}
+.btn-success:disabled, .btn-success.disabled {
+  color: #fff;
+  background-color: #198754;
+  border-color: #198754;
+}
+
+.btn-info {
+  color: #000;
+  background-color: #0dcaf0;
+  border-color: #0dcaf0;
+}
+.btn-info:hover {
+  color: #000;
+  background-color: #31d2f2;
+  border-color: #25cff2;
+}
+.btn-check:focus + .btn-info, .btn-info:focus {
+  color: #000;
+  background-color: #31d2f2;
+  border-color: #25cff2;
+  box-shadow: 0 0 0 0.25rem rgba(11, 172, 204, 0.5);
+}
+.btn-check:checked + .btn-info, .btn-check:active + .btn-info, .btn-info:active, .btn-info.active, .show > .btn-info.dropdown-toggle {
+  color: #000;
+  background-color: #3dd5f3;
+  border-color: #25cff2;
+}
+.btn-check:checked + .btn-info:focus, .btn-check:active + .btn-info:focus, .btn-info:active:focus, .btn-info.active:focus, .show > .btn-info.dropdown-toggle:focus {
+  box-shadow: 0 0 0 0.25rem rgba(11, 172, 204, 0.5);
+}
+.btn-info:disabled, .btn-info.disabled {
+  color: #000;
+  background-color: #0dcaf0;
+  border-color: #0dcaf0;
+}
+
+.btn-warning {
+  color: #000;
+  background-color: #ffc107;
+  border-color: #ffc107;
+}
+.btn-warning:hover {
+  color: #000;
+  background-color: #ffca2c;
+  border-color: #ffc720;
+}
+.btn-check:focus + .btn-warning, .btn-warning:focus {
+  color: #000;
+  background-color: #ffca2c;
+  border-color: #ffc720;
+  box-shadow: 0 0 0 0.25rem rgba(217, 164, 6, 0.5);
+}
+.btn-check:checked + .btn-warning, .btn-check:active + .btn-warning, .btn-warning:active, .btn-warning.active, .show > .btn-warning.dropdown-toggle {
+  color: #000;
+  background-color: #ffcd39;
+  border-color: #ffc720;
+}
+.btn-check:checked + .btn-warning:focus, .btn-check:active + .btn-warning:focus, .btn-warning:active:focus, .btn-warning.active:focus, .show > .btn-warning.dropdown-toggle:focus {
+  box-shadow: 0 0 0 0.25rem rgba(217, 164, 6, 0.5);
+}
+.btn-warning:disabled, .btn-warning.disabled {
+  color: #000;
+  background-color: #ffc107;
+  border-color: #ffc107;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #dc3545;
+  border-color: #dc3545;
+}
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bb2d3b;
+  border-color: #b02a37;
+}
+.btn-check:focus + .btn-danger, .btn-danger:focus {
+  color: #fff;
+  background-color: #bb2d3b;
+  border-color: #b02a37;
+  box-shadow: 0 0 0 0.25rem rgba(225, 83, 97, 0.5);
+}
+.btn-check:checked + .btn-danger, .btn-check:active + .btn-danger, .btn-danger:active, .btn-danger.active, .show > .btn-danger.dropdown-toggle {
+  color: #fff;
+  background-color: #b02a37;
+  border-color: #a52834;
+}
+.btn-check:checked + .btn-danger:focus, .btn-check:active + .btn-danger:focus, .btn-danger:active:focus, .btn-danger.active:focus, .show > .btn-danger.dropdown-toggle:focus {
+  box-shadow: 0 0 0 0.25rem rgba(225, 83, 97, 0.5);
+}
+.btn-danger:disabled, .btn-danger.disabled {
+  color: #fff;
+  background-color: #dc3545;
+  border-color: #dc3545;
+}
+
+.btn-light {
+  color: #000;
+  background-color: #f8f9fa;
+  border-color: #f8f9fa;
+}
+.btn-light:hover {
+  color: #000;
+  background-color: #f9fafb;
+  border-color: #f9fafb;
+}
+.btn-check:focus + .btn-light, .btn-light:focus {
+  color: #000;
+  background-color: #f9fafb;
+  border-color: #f9fafb;
+  box-shadow: 0 0 0 0.25rem rgba(211, 212, 213, 0.5);
+}
+.btn-check:checked + .btn-light, .btn-check:active + .btn-light, .btn-light:active, .btn-light.active, .show > .btn-light.dropdown-toggle {
+  color: #000;
+  background-color: #f9fafb;
+  border-color: #f9fafb;
+}
+.btn-check:checked + .btn-light:focus, .btn-check:active + .btn-light:focus, .btn-light:active:focus, .btn-light.active:focus, .show > .btn-light.dropdown-toggle:focus {
+  box-shadow: 0 0 0 0.25rem rgba(211, 212, 213, 0.5);
+}
+.btn-light:disabled, .btn-light.disabled {
+  color: #000;
+  background-color: #f8f9fa;
+  border-color: #f8f9fa;
+}
+
+.btn-dark {
+  color: #fff;
+  background-color: #212529;
+  border-color: #212529;
+}
+.btn-dark:hover {
+  color: #fff;
+  background-color: #1c1f23;
+  border-color: #1a1e21;
+}
+.btn-check:focus + .btn-dark, .btn-dark:focus {
+  color: #fff;
+  background-color: #1c1f23;
+  border-color: #1a1e21;
+  box-shadow: 0 0 0 0.25rem rgba(66, 70, 73, 0.5);
+}
+.btn-check:checked + .btn-dark, .btn-check:active + .btn-dark, .btn-dark:active, .btn-dark.active, .show > .btn-dark.dropdown-toggle {
+  color: #fff;
+  background-color: #1a1e21;
+  border-color: #191c1f;
+}
+.btn-check:checked + .btn-dark:focus, .btn-check:active + .btn-dark:focus, .btn-dark:active:focus, .btn-dark.active:focus, .show > .btn-dark.dropdown-toggle:focus {
+  box-shadow: 0 0 0 0.25rem rgba(66, 70, 73, 0.5);
+}
+.btn-dark:disabled, .btn-dark.disabled {
+  color: #fff;
+  background-color: #212529;
+  border-color: #212529;
+}
+</style>
+
+
---
+++ src/secret.js
@@ -0,0 +1,3 @@
+export const apiUrl = 'wss://ws.blockchain.info/inv';
+export const apiSubscribe = {"op":"unconfirmed_sub"};
+export const apiUnSubscribe = {"op":"unconfirmed_unsub"};
